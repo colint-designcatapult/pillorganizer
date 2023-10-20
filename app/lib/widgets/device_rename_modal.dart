b76edce82e943d41fd97cf72c33ada7638e2fc2d
Pierre-Etienne Morin <106545968+Pierre-EtienneM@users.noreply.github.com>
2023-10-20 16:24:26 -0400

    fix: settings rename keyboard not saving & validator (#43)

--- app/lib/widgets/device_rename_modal.dart
+++ app/lib/widgets/device_rename_modal.dart
@@ -1,3 +1,4 @@
+import 'package:app/api/device.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +15,7 @@ class ChangeDeviceNameDialog extends StatefulWidget {
 }
 
 class _ChangeDeviceNameDialogState extends State<ChangeDeviceNameDialog> {
-  final formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   bool enableOK = false;
   String? value;
 
@@ -23,138 +24,148 @@ class _ChangeDeviceNameDialogState extends State<ChangeDeviceNameDialog> {
     return Dialog(
       insetPadding: const EdgeInsets.all(20),
       child: Form(
-        key: formKey,
-        child: Consumer<SelectedDeviceProvider>(
-          builder: (context, provider, _) {
-            return Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              padding: const EdgeInsets.fromLTRB(10, 2, 10, 10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        onPressed: () {
+        key: _formKey,
+        child: FormSubmitCallback(
+          callback: _onSubmit,
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: const EdgeInsets.fromLTRB(10, 2, 10, 10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(PhosphorIcons.x_bold),
+                      color: const Color(0XFF101828),
+                    ),
+                  ],
+                ),
+                const Column(
+                  children: [
+                    Icon(
+                      PhosphorIcons.hard_drives,
+                      size: 48,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  "Change device name",
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                const SizedBox(height: 8),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    "Please enter the desired new device name right below:",
+                    style: Theme.of(context).textTheme.bodySmall,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: BasicPageTextFormField(
+                      labelText: Provider.of<SelectedDeviceProvider>(context,
+                                  listen: false)
+                              .device
+                              ?.name ??
+                          "Device name",
+                      onChanged: (newName) {
+                        value = newName;
+                      },
+                      onFieldSubmitted: (newName) => (value = newName),
+                      validator:
+                          Validatorless.required("A device name is required")),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 24),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: GestureDetector(
+                        onTap: () {
                           Navigator.of(context).pop();
                         },
-                        icon: const Icon(PhosphorIcons.x_bold),
-                        color: const Color(0XFF101828),
-                      ),
-                    ],
-                  ),
-                  const Column(
-                    children: [
-                      Icon(
-                        PhosphorIcons.hard_drives,
-                        size: 48,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "Change device name",
-                    style: Theme.of(context).textTheme.labelLarge,
-                  ),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      "Please enter the desired new device name right below:",
-                      style: Theme.of(context).textTheme.bodySmall,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: BasicPageTextFormField(
-                        labelText: provider.device?.name ?? "Device name",
-                        onChanged: (newName) {
-                          value = newName;
-                        },
-                        validator: Validatorless.required(
-                            "A device name is required")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 24),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                            child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Container(
-                              height: 44,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(12),
-                                border: Border.all(
-                                  color: const Color(0xFF206B8B),
-                                  width: 1.0,
-                                ),
-                              ),
-                              child: const Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    "Cancel",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(color: Color(0xFF206B8B)),
-                                  ))),
-                        )),
-                        const SizedBox(width: 12),
-                        Expanded(
-                            child: GestureDetector(
-                          onTap: () {
-                            _onSubmit();
-                          },
-                          child: Container(
-                              height: 44,
-                              decoration: BoxDecoration(
+                        child: Container(
+                            height: 44,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
                                 color: const Color(0xFF206B8B),
-                                borderRadius: BorderRadius.circular(12),
-                                border: Border.all(
-                                  color: const Color(0xFF206B8B),
-                                  width: 1.0,
-                                ),
+                                width: 1.0,
                               ),
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text("Save",
-                                      textAlign: TextAlign.center,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .displaySmall
-                                          ?.copyWith(
-                                            color: Colors.white,
-                                          )))),
-                        )),
-                      ],
-                    ),
+                            ),
+                            child: const Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "Cancel",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Color(0xFF206B8B)),
+                                ))),
+                      )),
+                      const SizedBox(width: 12),
+                      Expanded(
+                          child: GestureDetector(
+                        onTap: () {
+                          _onSubmit();
+                        },
+                        child: Container(
+                            height: 44,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF206B8B),
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
+                                color: const Color(0xFF206B8B),
+                                width: 1.0,
+                              ),
+                            ),
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text("Save",
+                                    textAlign: TextAlign.center,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displaySmall
+                                        ?.copyWith(
+                                          color: Colors.white,
+                                        )))),
+                      )),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
   void _onSubmit() async {
-    _save().then((value) => Navigator.of(context).pop());
+    if (_formKey.currentState?.validate() ?? false) {
+      _formKey.currentState?.save();
+      _save().then((value) => Navigator.of(context).pop());
+    }
   }
 
   Future<void> _save() async {
     if (value != null) {
       await Provider.of<SelectedDeviceProvider>(context, listen: false)
-          .updateName(value!);
+          .updateName(value!)
+          .then((value) =>
+              Provider.of<DeviceListProvider>(context, listen: false)
+                  .refresh());
     }
   }
 }
