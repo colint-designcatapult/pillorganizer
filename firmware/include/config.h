ea04fefc6c52f48cff50e11a8a3dd51df5f2f36c
chadpham <chadp@designcatpult.com>
2023-11-27 09:56:08 -0800

    Version 30
    Changed Wifi Provision Bluetooth Broadcasting with "CAB_XXXX"

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef FIRMWARE_REVISION
-#define FIRMWARE_REVISION       29
+#define FIRMWARE_REVISION       30
 #endif
 
 #ifndef FIRMWARE_BUILD          
@@ -9,7 +9,7 @@
 #endif
 
 #ifndef FIRMWARE_DATE          
-#define FIRMWARE_DATE           "11/21/2023"
+#define FIRMWARE_DATE           "11/27/2023"
 #endif
 
 #ifndef BOARD_REV
