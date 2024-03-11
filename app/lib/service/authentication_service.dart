c13baba6adf923e8ca616018c3280b357f889913
Pierre-Etienne Morin <pe.morin@t-b.ca>
2024-03-11 14:27:41 -0400

    fix: network issues error message

--- app/lib/service/authentication_service.dart
+++ app/lib/service/authentication_service.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProvisionError {
   static const String authGenericLoginError = "authGenericLoginError";
+  static const String authConnectionError = "authConnectionError";
   static const String authFailedError = "authFailedError";
 }
 
@@ -12,6 +13,8 @@ String authErrorMessage(BuildContext context, String authError) {
       return AppLocalizations.of(context)!.genericLoginError;
     case ProvisionError.authFailedError:
       return AppLocalizations.of(context)!.authError;
+    case ProvisionError.authConnectionError:
+      return AppLocalizations.of(context)!.authConnectionError;
     default:
       return AppLocalizations.of(context)!.genericError;
   }
