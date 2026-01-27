978dbbf82c6ac5eebc91f51682bef0a373a72720
Brendan Heinonen <brendanh@designcatapult.com>
2026-01-27 10:57:40 -0500

    Backend: use localstack for local AWS development, add secretsmanager integration

---
+++ backend/init-aws.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Use ca-central-1 Canadian region
+export AWS_DEFAULT_REGION=ca-central-1
+
+# Build secret in RDS format
+DB_SECRET_JSON=$(cat <<EOF
+{
+  "username": "postgres",
+  "password": "root",
+  "engine": "postgres",
+  "host": "localhost",
+  "port": 5432,
+  "dbname": "cabinet",
+  "dbInstanceIdentifier": "local-db"
+}
+EOF
+)
+
+awslocal secretsmanager create-secret \
+    --name /config/pillorganizer-backend/database \
+    --description "Local development database credentials" \
+    --secret-string "${DB_SECRET_JSON}"
