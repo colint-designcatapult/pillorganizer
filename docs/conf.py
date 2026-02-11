282837e5bb1d38c8d97a73c79c6e87a30b908bea
Brendan Heinonen <brendanh@designcatapult.com>
2026-02-11 14:57:24 -0500

    Reqs: add docxbuilder to build requirements as word doc

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Design Catapult, Inc'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['rst2pdf.pdfbuilder']
+extensions = ['rst2pdf.pdfbuilder', 'docxbuilder']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,5 +26,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-
+# PDF/docx export
 pdf_documents = [('index', u'requirements', u'Health-E Pill Organizer Requirements', u'Design Catapult, Inc'),]
+
+docx_documents = [('index', 'requirements.docx', {
+    'title': 'Health-E Pill Organizer Requirements',
+    'author': 'Design Catapult, Inc',
+    'subject': 'Requirements',
+    'keywords': ['sphinx']
+}, True)]
+docx_pagebreak_before_section = 2
+docx_pagebreak_after_table_of_contents = 1
