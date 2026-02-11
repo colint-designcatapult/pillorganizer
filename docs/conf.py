9767ff04d578613fa74e31e7ac53b5c5953ec782
Brendan Heinonen <brendanh@designcatapult.com>
2026-02-11 15:07:41 -0500

    Reqs: update docx export to include version

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# This function runs after -D overrides are applied so we can inject version into the
+# docx config.
+def setup(app):
+    def update_docx_subject(app, config):
+        
+        if config.docx_documents:
+            for doc in config.docx_documents:
+                metadata = doc[2]
+                
+                # Append the version from the command line (-D version=...)
+                # config.version will now hold the correct GIT_SHA
+                metadata['subject'] = f"Version {config.version}"
+
+    # Connect this function to the 'config-inited' event
+    app.connect('config-inited', update_docx_subject)
+
 project = 'Health-E Pill Organizer'
 copyright = '2026, Design Catapult, Inc'
 author = 'Design Catapult, Inc'
@@ -31,9 +47,8 @@ pdf_documents = [('index', u'requirements', u'Health-E Pill Organizer Requiremen
 
 docx_documents = [('index', 'requirements.docx', {
     'title': 'Health-E Pill Organizer Requirements',
-    'author': 'Design Catapult, Inc',
-    'subject': 'Requirements',
-    'keywords': ['sphinx']
+    'creator': 'Design Catapult, Inc',
+    'subject': 'Unversioned'
 }, True)]
 docx_pagebreak_before_section = 2
 docx_pagebreak_after_table_of_contents = 1
