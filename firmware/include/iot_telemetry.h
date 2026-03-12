0a41d4f1e5ce96e360fa72d68a551896c3f0167b
Brendan Heinonen <brendanh@designcatapult.com>
2026-03-12 15:51:12 -0400

    Firmware: push bin door state to MQTT (quick and dirty)

--- firmware/include/iot_telemetry.h
+++ firmware/include/iot_telemetry.h
@@ -10,4 +10,4 @@ esp_err_t iot_telemetry_start(void);
 /**
  * @brief Publishes all 14 bin states to /tenant/{id}/{device}/bins/state.
  */
-void iot_telemetry_publish_bin_state(void);
+void iot_telemetry_publish_bin_state(uint16_t);
