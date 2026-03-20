2a653d8fabfc4d9dbfeaae66d41c8291370ba13c
Brendan Heinonen <brendanh@designcatapult.com>
2026-03-20 14:41:10 -0400

    Firmware: use RTC timer for relative timestamps

--- firmware/main/include/rtc.h
+++ firmware/main/include/rtc.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <stdint.h>
+#include <time.h>
 
 void app_rtc_init();
 void app_rtc_sync();
+
+typedef uint64_t rtc_relative_time_t;
+
+// Gets a timestamp that can be converted to UTC time regardless of RTC sync status
+rtc_relative_time_t app_rtc_get_relative_timestamp();
+
+// Converts a relative timestamp to UTC time, given that the RTC has been synced
+time_t app_rtc_calc_utc_time(rtc_relative_time_t rtc_time);
--- firmware/main/rtc.c
+++ firmware/main/rtc.c
@@ -5,10 +5,14 @@
 #include "esp_netif_sntp.h"
 #include "esp_sntp.h"
 #include "supervisor.h"
+#include <esp_timer.h>
+#include <esp32/rtc.h>
+#include "assert.h"
 
 #define TAG "RTC"
 
-RTC_DATA_ATTR time_t last_sntp_sync_time = 0;
+RTC_DATA_ATTR static time_t last_sntp_sync_time = 0;
+RTC_DATA_ATTR static uint64_t last_sync_rtc_time = 0;
 
 void app_rtc_time_sync_notification_cb(struct timeval *tv)
 {
@@ -30,6 +34,7 @@ void app_rtc_time_sync_notification_cb(struct timeval *tv)
 
     // Set last sync time to RTC memory
     last_sntp_sync_time = now;
+    last_sync_rtc_time = esp_rtc_get_time_us();
 
     // Notify system of time sync
     ESP_ERROR_CHECK(supervisor_submit_event(EVENT_TIME_SYNCED));
@@ -53,4 +58,23 @@ void app_rtc_sync()
 {
     ESP_LOGI(TAG, "Forcing sync with SNTP");
     sntp_restart();
+}
+
+rtc_relative_time_t app_rtc_get_relative_timestamp()
+{
+    return esp_rtc_get_time_us();
+}
+
+time_t app_rtc_calc_utc_time(rtc_relative_time_t rtc_time)
+{
+    // Time must be synced to use this function
+    assert(last_sync_rtc_time != 0);
+
+    // Calculate how many microseconds before the sync the event occurred
+    int64_t time_diff_us = last_sync_rtc_time - rtc_time;
+    
+    // Convert the difference to seconds and subtract from the synced UTC time
+    time_t event_utc_time = last_sntp_sync_time - (time_t)(time_diff_us / 1000000ULL);
+    
+    return event_utc_time;
 }
