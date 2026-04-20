38227ede079b4ed1272512423438004c7731bb86
Brendan Heinonen <brendanh@designcatapult.com>
2026-04-20 18:39:53 -0400

    Firmware: reset mux door state on timeout, resolves #282

--- firmware/main/include/mux_io.h
+++ firmware/main/include/mux_io.h
@@ -1,6 +1,6 @@
-/* 
+/*
  * Sensor MUX Driver & I/O
- * 
+ *
  * Code for reading sensor data via the CD4067B MUX.
  */
 #pragma once
@@ -10,6 +10,9 @@
 void mux_init();
 void mux_fresh_boot();
 void mux_wake_deep_sleep();
-// Returns true if the system should boot normally, false if it should go back to sleep
+// Returns true if the system should boot normally, false if it should go back
+// to sleep
 bool mux_wake_deep_sleep_early();
 void mux_prep_deep_sleep();
+
+void mux_force_door_state_reset(int door_id);
